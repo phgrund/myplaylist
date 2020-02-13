8220111b333652384b37bc6c456adc5a8e6d70f5
phgrund <paulinhogrund@gmail.com>
2020-02-13 20:55:54 -0300

    :rocket: Anime CRUD finish

--- app/Http/Controllers/AnimeController.php
+++ app/Http/Controllers/AnimeController.php
@@ -11,10 +11,34 @@ class AnimeController extends Controller
         return response()->json(Anime::all());
     }
 
+    public function show($id) {
+        $anime = Anime::find($id);
+
+        return response()->json($anime);
+    }
+
     public function store() {
         $data = $this->validateData();
 
         $anime = Anime::create($data);
+
+        return response()->json($anime);
+    }
+
+    public function update($id) {
+        $anime = Anime::find($id);
+
+        $data = $this->validateData($anime);
+
+        $anime->update($data);
+
+        return response()->json($anime);
+    }
+
+    public function destroy($id) {
+        $anime = Anime::find($id);
+
+        $anime->delete();
 
         return response()->json($anime);
     }
--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,10 @@
 use Illuminate\Http\Request;
 
 Route::get('/animes', 'AnimeController@index');
+Route::get('/animes/{id}', 'AnimeController@show');
 Route::post('/animes', 'AnimeController@store');
+Route::put('/animes/{id}', 'AnimeController@update');
+Route::delete('/animes/{id}', 'AnimeController@destroy');
 
 Route::group([
   'middleware' => 'api',
